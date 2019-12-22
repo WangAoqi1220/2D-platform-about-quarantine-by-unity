ec43944cb026aff364fa615c38f4ff551056aa28
Taoist_Yu <294196463@qq.com>
2019-12-22 14:16:53 +0800

    Player

---
+++ 2D platform/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+	[RequireComponent(typeof(PlayerMove))]
+	public class Player : MonoBehaviour
+	{
+		#region 子组件
+		public PlayerMove Move { get; private set; }
+		#endregion
+
+		#region MonoBehaviour生命周期
+		private void Awake()
+		{
+			//初始化对象组件
+			this.Move = gameObject.GetComponent<PlayerMove>();
+		
+			
+		}
+
+		private void Start()
+		{
+			gameObject.tag = "Player";
+		}
+
+		#endregion
+
+	}
+}
+
+
