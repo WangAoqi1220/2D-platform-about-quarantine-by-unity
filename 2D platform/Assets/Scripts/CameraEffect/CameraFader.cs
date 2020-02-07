7f325d32b328d03ae4b48c4c3fdf2e76859c27bd
AoqiWang <1017802402@qq.com>
2020-02-07 16:08:30 +0800

    SceneChange

---
+++ 2D platform/Assets/Scripts/CameraEffect/CameraFader.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFader : MonoBehaviour
+{
+    public Camera[] Cameras;
+
+    public Color[] CamCols = null;
+
+    public float FadeTime = 2.0f;
+
+    public Material Mat = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Camera c in Cameras)
+            c.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);//摄像机的目标渲染纹理
+    }
+
+    private void OnPostRender()//相机完成渲染后调用该方法
+    {
+        Rect ScreenRct = new Rect(0, 0, Screen.width, Screen.height);
+
+        Rect SourceRect = new Rect(0, 1, 1, -1);
+
+        for(int i = 0; i < Cameras.Length; i++)
+        {
+            Cameras[i].Render();
+            GL.PushMatrix();
+            GL.LoadPixelMatrix();
+            Graphics.DrawTexture(ScreenRct, Cameras[i].targetTexture, SourceRect, 0, 0, 0, 0, CamCols[i]);
+            GL.PopMatrix();
+        }
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)//相机完成图片渲染后调用该方法
+    {
+        Graphics.Blit(src, dst, Mat);//将源纹理src复制到目标dst中，以使用着色器渲染纹理。这主要用于实现mat后处理效果。
+
+    }
+
+    public IEnumerator Fade(Color From, Color To, float TotalTime)
+    {
+        float ElapsedTime = 0f;
+        while(ElapsedTime <= TotalTime)
+        {
+            CamCols[1] = Color.Lerp(From, To, ElapsedTime/TotalTime);
+
+            yield return null;
+
+            ElapsedTime += Time.deltaTime;
+
+            CamCols[1] = Color.Lerp(From, To, 1f);
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StopAllCoroutines();//停止所有协程
+
+            if (CamCols[1].a <= 0f)
+                StartCoroutine(Fade(CamCols[1], new Color(0.5f, 0.5f, 0.5f, 1f), FadeTime));
+            else
+                StartCoroutine(Fade(CamCols[1], new Color(0.5f, 0.5f, 0.5f, 0f), FadeTime));
+
+        }
+    }
+}
---
+++ 2D platform/Assets/Scripts/CameraEffect/CameraShake.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private Transform ThisTransform = null;
+
+    public float ShakeTime = 2.0f;
+
+    public float ShakeAmount = 3.0f;
+
+    public float ShakeSpeed = 2.0f;
+
+    void Start()
+    {
+        ThisTransform = GetComponent<Transform>();
+
+        StartCoroutine(Shake());
+    }
+
+    public IEnumerator Shake()
+    {
+        Vector3 OrigPosition = ThisTransform.localPosition;
+        float ElapsedTime = 0.0f;
+        while(ElapsedTime < ShakeTime)
+        {
+            Vector3 RandomPoint = OrigPosition + Random.insideUnitSphere * ShakeAmount;
+            ThisTransform.localPosition = Vector3.Lerp(ThisTransform.localPosition, RandomPoint, Time.deltaTime * ShakeSpeed);
+
+            yield return null;
+            ElapsedTime += Time.deltaTime;
+        }
+
+        ThisTransform.localPosition = OrigPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
